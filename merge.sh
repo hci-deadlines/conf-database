dcec755666469079182c84900c2d1ac68e5a3c54
Andrea <andrea@kaist.ac.kr>
2024-02-08 17:59:47 +1000

    Fixing names

--- merge.sh
+++ merge.sh
@@ -2,4 +2,4 @@
 # get current directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # merge files
-awk 'FNR==1{print ""}1' $SCRIPT_DIR/conferences/*.yml > $SCRIPT_DIR/autogen-conferences.yml
+awk 'FNR==1{print ""}1' $SCRIPT_DIR/conferences/*.yml > $SCRIPT_DIR/conferences.yml
